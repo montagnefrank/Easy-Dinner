956a9e2f351c9d8a3fe468ddbb86b0363c05a8ef
Bayman Burton <burton@burtonservers.com>
2018-04-09 17:33:18 -0500

    Update
    Se actualizan los cambios de Dorian con respecto a la gestion de la construcci'on del menu.

--- assets/conn.php
+++ assets/conn.php
@@ -1,16 +1,16 @@
 <?php
 
 // Conexion a la base de datos PRODUCCION
-//define('DB_HOST', 'localhost');
-//define('DB_USER', 'root');
-//define('DB_PASSWORD', 'W1nnts3rv3r');
-//define('DB_DATABASE', 'dirulo');
-
-// Conexion a la base de datos LOCAL
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASSWORD', 'vagrant');
+define('DB_PASSWORD', 'W1nnts3rv3r');
 define('DB_DATABASE', 'dirulo');
+
+// Conexion a la base de datos LOCAL
+//define('DB_HOST', 'localhost');
+//define('DB_USER', 'root');
+//define('DB_PASSWORD', 'vagrant');
+//define('DB_DATABASE', 'dirulo');
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
 mysqli_set_charset($conn, "utf8");
